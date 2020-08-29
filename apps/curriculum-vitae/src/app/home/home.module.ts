032fb48db20e65e67c2500e545973929ab48bfdf
Cédric Moulard <cedric.moulard@gmail.com>
2020-08-29 14:39:06 +0200

    chore: update to angular 10

--- apps/curriculum-vitae/src/app/home/home.module.ts
+++ apps/curriculum-vitae/src/app/home/home.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HomeComponent } from "./home.component";
 import { HomeRoutingModule } from "./home-routing.module";
-import { MatButtonModule } from "@angular/material";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
   declarations: [HomeComponent],
